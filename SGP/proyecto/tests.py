7fe5d306e24c042cc49deaf921d0b1fca3141261
andrea <andy.benitez09@gmail.com>
2015-04-10 09:49:13 -0300

    test proyecto

--- SGP/proyecto/tests.py
+++ SGP/proyecto/tests.py
@@ -3,106 +3,83 @@ from django.contrib.auth import SESSION_KEY
 from django.test import Client
 from django.test import TestCase
 from django.contrib.auth.models import User
-from models import Proyecto, FlujoProyecto, Equipo
+from models import Proyecto
+
 
 class SGPTestCase(TestCase):
 
-    fixtures = ["proyectos_testmaker"]
+    fixtures = ["clientes_testmaker"]
 
-    def test_crear_usuario(self):
+    def test_crear_proyecto(self):
         '''
-        Test para la creacion de un usuario con contrasenha
+        Test para la creacion de un proyecto
+        @author: Andrea Benitez
         '''
-        P = User.objects.create_user('testuser', 'test@example.com', 'testpw')
+        c = Client()
+        c.login(username='admin', password='admin1')
 
-        self.assertTrue(u.has_usable_password())
-        self.assertFalse(u.check_password('bad'))
-        self.assertTrue(u.check_password('testpw'))
+        resp = c.post('/proyectos/crear_proyecto/',{"nombre": "proyectoTest", "estado": "Iniciado", "fecha_inicio": "2015-04-08", "duracion_estimada": 10, "descripcion": "Proyecto test",  "is_active": "TRUE"})
+        self.assertTrue(resp.status_code, 200)
+        print ('\n Se crea correctamente el proyecto')
 
-        # Test para contrasenha incorrecta
-        u.set_unusable_password()
-        u.save()
-        self.assertFalse(u.check_password('testpw'))
-        self.assertFalse(u.has_usable_password())
-        u.set_password('testpw')
-        self.assertTrue(u.check_password('testpw'))
-        u.set_password(None)
-        self.assertFalse(u.has_usable_password())
-
-        # Test para creacion sin password
-        u2 = User.objects.create_user('testuser2', 'test2@example.com')
-        self.assertFalse(u2.has_usable_password())
 
     def test_inicio(self):
         '''Test para ver si puede entrar a la pagina de inicio'''
-        resp = self.client.get('/autenticacion/')
+        resp = self.client.get('/proyectos/')
+        self.assertEqual(resp.status_code, 200)
+        print ('\n Se ingresa correctamente al modulo de proyectos')
+
+
+    def test_listar_proyecto(self):
+        '''
+         Test para crear un proyecto y ver si lo lista correctamente
+         @author: Andrea Benitez
+        '''
+        f = Client()
+        f.login(username='admin', password='admin1')
+        resp = f.post('/proyectos/crear_proyecto/',{"nombre": "proyectoTest2", "estado": "Iniciado", "fecha_inicio": "2015-04-08", "duracion_estimada": 10, "descripcion": "Proyecto test",  "is_active": "TRUE"})
+        self.assertTrue(resp.status_code,200)
+        #f = Cliente.objects.create (nombre='testcliente2', ruc=1234, numeroTelefono=123456, representante=2)
+
+        #f = Client()
+        #c.login(username='admin', password='admin1')
+        resp = f.get('/proyectos/')
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue('lista_proyectos' in resp.context)
+        self.assertEqual([cli.pk for cli in resp.context['lista_proyectos']], [1,2])
+        proyecto1 = resp.context['lista_proyectos'][1]
+        self.assertEqual(proyecto1.nombre, 'proyectoTest2')
+        self.assertEqual(proyecto1.estado, 'Iniciado')
+        self.assertEqual(proyecto1.fecha_inicio, '2015-04-08')
+        self.assertEqual(proyecto1.duracion_estimada, '10')
+        self.assertEqual(proyecto1.descripcion, 'Proyecto test')
+        self.assertEqual(proyecto1.is_active, 'TRUE')
+
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue('lista_proyectos' in resp.context)
+        self.assertEqual([cli.pk for cli in resp.context['lista_proyectos']], [1,2])
+        proyecto1 = resp.context['lista_proyectos'][1]
+        self.assertEqual(proyecto1.nombre, 'proyectoTest')
+        self.assertEqual(proyecto1.estado, 'Iniciado')
+        self.assertEqual(proyecto1.fecha_inicio, '2015-04-08')
+        self.assertEqual(proyecto1.duracion_estimada, '10')
+        self.assertEqual(proyecto1.descripcion, 'Proyecto test')
+        self.assertEqual(proyecto1.is_active, 'TRUE')
+
+
+
+    def test_detalle_proyecto(self):
+        '''
+        Test para visualizar los detalles de un Proyecto
+        @author: Andrea Benitez
+        '''
+        f = Client()
+        f.login(username='admin', password='admin1')
+        resp = f.post('/proyectos/crear_proyecto/',{"nombre": "proyectoTest3", "estado": "Iniciado", "fecha_inicio": "2015-04-08", "duracion_estimada": 10, "descripcion": "Proyecto test",  "is_active": "TRUE"})
+
+        resp = f.get('/clientes/consultar/2/')
         self.assertEqual(resp.status_code, 200)
 
-    def login(self, password='testpw'):
-        '''
-        Test para el login
-        '''
-        response = self.client.post('/login/', {
-            'username': 'testuser',
-            'password': password,
-            })
-        self.assertTrue(SESSION_KEY in self.client.session)
-        return response
-
-    def test_listar_usuarios(self):
-        '''
-         Test para crear un usuario y ver si lo lista correctamente
-        '''
-
-        usuario = User.objects.create_user('testuser', 'test@example.com', 'testpw')
-        c = Client()
-        c.login(username='admin', password='admin1')
-        resp = c.get('/usuarios/')
-        self.assertEqual(resp.status_code, 200)
-        self.assertTrue('lista_usuarios' in resp.context)
-        self.assertEqual([usu.pk for usu in resp.context['lista_usuarios']], [2,1])
-        usuario1 = resp.context['lista_usuarios'][0]
-        self.assertEqual(usuario1.username, 'testuser')
-        self.assertEqual(usuario1.email, 'test@example.com')
-
-        self.assertEqual(resp.status_code, 200)
-        self.assertTrue('lista_usuarios' in resp.context)
-        self.assertEqual([usu.pk for usu in resp.context['lista_usuarios']], [2,1])
-        usuario1 = resp.context['lista_usuarios'][1]
-        self.assertEqual(usuario1.username, 'admin')
-        self.assertEqual(usuario1.email, 'admin@admin.com')
 
 
-
-    def test_detalle_usuarios(self):
-        '''
-        Test para visualizar los detalles de un usuario
-        '''
-        usuario = User.objects.create_user('testuser', 'test@example.com', 'testpw')
-        c = Client()
-        c.login(username='testuser', password='testpw')
-        resp = c.get('/usuarios/consultar/2/')
-        self.assertEqual(resp.status_code, 200)
-        self.assertEqual(resp.context['perfil'].pk, 2)
-        self.assertEqual(resp.context['perfil'].username, 'testuser')
-
-    def test_modificar_usuarios(self):
-        '''
-        Test para cambiar el estado de un usuario
-        '''
-        usuario2 = User.objects.create_user('testuser3', 'test@example.com', 'testpw')
-        c = Client()
-        c.login(username='testuser3', password='testpw')
-        resp = self.client.post('/usuarios/modificar/1?')
-        self.assertEqual(resp.status_code, 301)
-
-    def test_logout(self):
-        '''
-        Test para el logout
-        '''
-        usuario = User.objects.create_user('testuser', 'test@example.com', 'testpw')
-        c = Client()
-        c.login(username='testuser', password='testpw')
-        response = c.get('/autenticacion/')
-        self.assertEqual(response.status_code, 200)
-        self.assertTrue(SESSION_KEY not in self.client.session)
