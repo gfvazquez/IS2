17ed920ba2ebbdeada880092f150d3e5e8e0e3db
andrea <andy.benitez09@gmail.com>
2015-05-16 01:06:40 -0400

    cron.sh

---
+++ SGP/scripts/cron.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export PGPASSWORD="postgres"
+echo "Inicio: Actualizacion de estado de Sprint"
+psql -c "update sprint_sprint set estado='Finalizado' where fechafin <current_date" -d is2 -U postgres
+echo "Fin: Actualizacion de estado de Sprint"
+
+echo "Inicio: Actualizacion de estado de Flujo en la tabla flujoProyecto"
+psql -c "update proyecto_flujoproyecto set estado='HalfDone' from sprint_sprint 
+where proyecto_flujoproyecto.sprint_id = sprint_sprint.id and sprint_sprint.estado='Finalizado' and proyecto_flujoproyecto.estado not ilike 'Done' and proyecto_flujoproyecto.estado not ilike 'Inactivo'" -d is2 -U postgres
+echo "Fin: Actualizacion de estado de Flujo en la tabla flujoProyecto"
+
+
+echo "Inicio: Actualizacion del estado de los US de los sprints que no se finalizaron, estos US deben asignarse al sgte Sprint"
+psql -c "update userstory_userstory set estado = 'Incompleto' where userstory_userstory.sprint_id = 
+(select proyecto_flujoproyecto.sprint_id 
+	from proyecto_flujoproyecto 
+	where estado='HalfDone') and userstory_userstory.estado not ilike 'Validado'" -d is2 -U postgres
+echo "Fin: Actualizacion del estado de los US de los sprints que no se finalizaron, estos US deben asignarse al sgte Sprint"
+
+
+
