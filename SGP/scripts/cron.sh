5394b347a038fdd5f99f64099ae932e6310500a4
andrea <andy.benitez09@gmail.com>
2015-05-16 02:51:36 -0400

    En la vista de consultar_kanban se avisa si se han concluido las actividades del flujo

--- SGP/scripts/cron.sh
+++ SGP/scripts/cron.sh
@@ -6,17 +6,15 @@ psql -c "update sprint_sprint set estado='Finalizado' where fechafin <current_da
 echo "Fin: Actualizacion de estado de Sprint"
 
 echo "Inicio: Actualizacion de estado de Flujo en la tabla flujoProyecto"
-psql -c "update proyecto_flujoproyecto set estado='HalfDone' from sprint_sprint 
+psql -c "update proyecto_flujoproyecto set estado='HalfDone' from sprint_sprint
 where proyecto_flujoproyecto.sprint_id = sprint_sprint.id and sprint_sprint.estado='Finalizado' and proyecto_flujoproyecto.estado not ilike 'Done' and proyecto_flujoproyecto.estado not ilike 'Inactivo'" -d is2 -U postgres
 echo "Fin: Actualizacion de estado de Flujo en la tabla flujoProyecto"
 
 
 echo "Inicio: Actualizacion del estado de los US de los sprints que no se finalizaron, estos US deben asignarse al sgte Sprint"
-psql -c "update userstory_userstory set estado = 'Incompleto' where userstory_userstory.sprint_id = 
-(select proyecto_flujoproyecto.sprint_id 
-	from proyecto_flujoproyecto 
-	where estado='HalfDone') and userstory_userstory.estado not ilike 'Validado'" -d is2 -U postgres
+psql -c "update userstory_userstory set estado = 'Incompleto'
+where userstory_userstory.estado not ilike 'Validado' and
+userstory_userstory.sprint_id in (select proyecto_flujoproyecto.sprint_id
+	from proyecto_flujoproyecto
+	where estado='HalfDone')" -d is2 -U postgres
 echo "Fin: Actualizacion del estado de los US de los sprints que no se finalizaron, estos US deben asignarse al sgte Sprint"
-
-
-
